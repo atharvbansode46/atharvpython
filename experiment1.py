2365ea0d10ab86a2770c414c2a08c3ec8fa05b3f
atharv <bansodeatharv07@gmail.com>
2026-02-13 15:10:28 +0530

    Add files via upload

---
+++ experiment1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 14:42:27 2026
+
+@author: User
+"""
+
+name=input("enter your name:")
+age=input("enter your age:")
+student_class=input("enter your class:")
+division=input("enter your division:")
+roll_no=input("enter your roll number:")
+batch=input("enter your batch:")
+phone_no=input("enter your phone number:")
+blood_group=input("enter your blood group:")
+adress=input("enter your adress:")
+collage=input("enter your collage name :")
+city=input("enter your city:")
+print("\n -----student in formation -------")
+
+
+
+
+
+
+print("name:",name)
+print("age:",age)
+print("student class:",student_class)
+print("division:",division)
+print("roll number:",roll_no)
+print("batch:",batch)
+print("phone number:",phone_no)
+print("blood group:",blood_group)
+print("adress:",adress)
+print("collage:",college)
+print("city:",city)
