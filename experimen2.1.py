2f56fea0609d65fdd46d3406d03d692718fa283b
atharv <bansodeatharv07@gmail.com>
2026-02-13 15:51:46 +0530

    Add files via upload

---
+++ experimen2.1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 13 15:42:43 2026
+
+@author: User
+"""
+
+year=int(input("enter year:"))
+if(year % 400 ==0) or (year % 4== 0 and year % 100 !=0):
+    print("leap year")
+else:
+    print("not a leap year")
